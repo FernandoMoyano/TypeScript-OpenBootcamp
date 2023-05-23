6210b872779227f0063ecbbf98f25835fe30cf5f
FernandoMoyano <fernandomoyano21@gmail.com>
2023-05-23 19:41:44 -0300

    sintaxis

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,11 @@ console.log("Hola Martin")
 
 //esto es uncomentario tanto para js como ts
  
+/* declaracion de variables: */
+var nombre: string ="Martin";//indicamos el tipo de variable
+console.log(`Hola ${nombre}`);
+
+let email="fernandomoyano21@gmail.com";
+console.log(`El email de ${nombre} es ${email}`);
+
+const PI:number=3.1416;//tipo number
